3879ef1b4e2e0091e98fb4348c7207dc77c3057d
saikatbairagi <saikatbairagi@saikats-mbp>
2020-11-05 09:07:52 +0530

    Taylor series commit

--- LeetCode/src/com/leetcode/recursion/TaylorSeries.java
+++ LeetCode/src/com/leetcode/recursion/TaylorSeries.java
@@ -2,9 +2,21 @@ package com.leetcode.recursion;
 
 public class TaylorSeries {
 
+	public double p = 1, f = 1;
+
+	public double taylorSeries(int num, int term) {
+		double result=0;
+		if (term == 0)
+			return 1;
+		result = taylorSeries(num, term - 1);
+		p = p * num;
+		f = f * term;
+		return result + p/f;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("saikat");
+		// time complexity--> O(n2)
+		System.out.println("Taylor series--> "+new TaylorSeries().taylorSeries(1, 10));
 
 	}
 
